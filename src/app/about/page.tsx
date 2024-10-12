69244b7904dd86e089002df73a051e8da5c1b44c
Cole Gawin <colegawin@gmail.com>
2024-10-11 17:45:47 -0700

    feat: add fireside-chats and about placeholders

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex h-[100vh] w-full bg-black">
+      <h1 className="m-auto text-center font-serif text-3xl text-white">Coming soon...</h1>
+    </div>
+  );
+}
---
+++ src/app/fireside-chats/page.tsx
@@ -0,0 +1,7 @@
+export default function FiresideChats() {
+  return (
+    <div className="flex h-[100vh] w-full bg-black">
+      <h1 className="m-auto text-center font-serif text-3xl text-white">Coming soon...</h1>
+    </div>
+  );
+}
